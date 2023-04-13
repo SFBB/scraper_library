3be11ce1a474c2f149a5bbf8b6b1038649e49d89
SFBB <anzhe990206@outlook.com>
2023-04-13 22:14:58 +0800

    Fix tqdm stil ldisplayed even finished bug.

--- src/view_base.py
+++ src/view_base.py
@@ -24,18 +24,33 @@ class view_bash(view_base):
     def __intt__(self, **kwargs):
         super(view_bash, self).__init__(**kwargs)
         self.bar = None
+        self.length = 0
+        self.progress = 0
 
     def __del__(self):
-        self.bar.disable()
+        if self.bar is not None:
+            self.bar.close()
 
     def print(self, text):
         print(text)
 
     def bar_update(self, delta: int):
-        self.bar.update(delta)
+        if self.bar is not None:
+            self.bar.update(delta)
+
+            self.progress += delta
+            if self.progress >= self.length:
+                self.bar.close()
+                self.bar = None
+                self.length = 0
+                self.progress = 0
 
     def bar_description(self, text):
-        self.bar.set_description(text)
+        if self.bar is not None:
+            self.bar.set_description(text)
 
     def new_bar(self, length: int):
         self.bar = tqdm(total = length)
+        self.length = length
+        self.progress = 0
+
